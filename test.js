78dcdaa06ed95b5d79a1c48eec7bc42ad5415a38
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2019-03-10 23:16:28 +0100

    introduced cool stuff (#28)

--- test.js
+++ test.js
@@ -8,15 +8,16 @@ const expect = require('expect');
   const navigationPromise = page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 0 })
 
   await page.setViewport({ width: 1024, height: 768 })
+
     console.log('√ puppeteer has launched')
 
 
-    /*
-    | -----------------------------------
-    | SELECTORS
-    | -----------------------------------
-    | selector collection
-    */
+/*
+ -----------------------------------
+ SELECTORS
+ -----------------------------------
+ selector collection
+*/
 
       const airFrom             = '#air-from'
       const airTo               = '#air-to'
@@ -27,20 +28,21 @@ const expect = require('expect');
       const deselectComparesite = '.hp-searchform-comparesite-selectnone'
       const airSubmit           = '#air-submit'
 
-  /*
-  | GIVEN I am on the homepage
-  */
+/*
+ GIVEN I am on the homepage
+*/
 
   await page.goto('https://www.liligo.fr/', { waitUntil: 'networkidle2', timeout: 0 })
+
     console.log('√ page is loaded successfully with all its assets')
 
-    /*
-    | -----------------------------------
-    | LOCATIONS
-    | -----------------------------------
-    | WHEN I set departure
-    | AND I set arrival
-    */
+/*
+ -----------------------------------
+ LOCATIONS
+ -----------------------------------
+ WHEN I set departure
+ AND I set arrival
+*/
 
   await page.waitForSelector(airFrom)
   await page.click(airFrom)
@@ -50,6 +52,7 @@ const expect = require('expect');
   await page.click(complocFirst)
   let airFromContent = await page.evaluate(el => el.value, await page.$(airFrom))
   expect(airFromContent).toBe('San Francisco,  CA, Etats-Unis (SFO)')
+
     console.log('√ departure is set ' + airFromContent)
 
 
@@ -59,15 +62,16 @@ const expect = require('expect');
   await page.click(complocSecond)
   let airToContent = await page.evaluate(el => el.value, await page.$(airTo))
   expect(airToContent).toBe('Paris, France (CDG)')
+
     console.log('√ arrival is set ' + airToContent)
 
 
-  /*
-  | -----------------------------------
-  | DATE PICKER
-  | -----------------------------------
-  | AND I set a new date
-  */
+/*
+ -----------------------------------
+ DATE PICKER
+ -----------------------------------
+ AND I set a new date
+*/
 
       const nowDate              = '.now'
       const actualDate           = '.actual'
@@ -81,50 +85,119 @@ const expect = require('expect');
   let airFromDateContent = await page.evaluate(el => el.innerText, await page.$(airFromDate))
   await page.waitForSelector(airToDate)
   let airToDateContent = await page.evaluate(el => el.innerText, await page.$(airToDate))
+
     console.log('√ default departure date is: ' + airFromDateContent)
     console.log('√ default arrival date is: ' + airToDateContent)
+
       await page.click(airFromDate)
       await page.waitForSelector(datePickerArrowRight)
       await page.click(datePickerArrowRight)
       await page.click(randomFutureDate)
         airFromDateContent = await page.evaluate(el => el.innerText, await page.$(airFromDate)) // format: 18 Avr. 2019 (jeudi)
         airToDateContent = await page.evaluate(el => el.innerText, await page.$(airToDate))     // format: 25 Avr. 2019 (jeudi)
+
           console.log('√ selected departure date is: ' + airFromDateContent)
           console.log('√ selected arrival date is: ' + airToDateContent)
+
             // validates date selection
             await page.click(airFromDate)
             let airFromDateSelectedMonth = await page.evaluate(el => el.innerText, await page.$(datePickerMonth1))
             let airFromDateSelectedDay = await page.evaluate(el => el.innerText, await page.$(actualDate))
               let airFromDateSelected = airFromDateSelectedDay + ' ' + airFromDateSelectedMonth // format: 18 Avril, 2019
+
             console.log('----> departure day selected: ' + airFromDateSelected)
+
               expect(airFromDateContent).toMatch(/19/)
+
               console.log('√ airFrom contains "19" \n')
+
             await page.click(airToDate)
             let airToDateSelectedMonth = await page.evaluate(el => el.innerText, await page.$(datePickerMonth1))
             let airToDateSelectedDay = await page.evaluate(el => el.innerText, await page.$(actualDate))
               let airToDateSelected = airToDateSelectedDay + ' ' + airToDateSelectedMonth     // format: 25 Avril, 2019
+
             console.log('----> arrival day selected: ' + airToDateSelected)
+
               expect(airToDateContent).toMatch(/19/)
+
               console.log('√ airTo contains "19" \n')
 
-            // clickouts from datePicker
-            await page.click('.hp-searcharea') // the searchform's background
+// prepare arrays from route location eleemnts for result page validation
+let airFromContentArray = airFromContent.split(', ')
+console.log( airFromContentArray )
+let airToContentArray = airToContent.split(', ')
+console.log( airToContentArray )
+/*
+expected output:
+
+[ 'San Francisco', ' CA', 'Etats-Unis (SFO)' ]
+[ 'Paris', 'France (CDG)' ]
+
+*/
+
+/*
+ -----------------------------------
+ CLICKOUT (promise.race)
+ -----------------------------------
+ AND I clickout from datePicker
+*/
+
+                const clickoutHome = '.hp-searcharea' // the searchform's background on homepage
+                const clickoutSeo  = '.sc-searchform'  // the searchform's background on SEO (sc)
+
+
+                await Promise.race([
+                  page.waitForSelector(clickoutHome),
+                  page.waitForSelector(clickoutSeo)
+                ]);
+
+                if (await page.$(clickoutHome) !== null) {
+                    await page.click(clickoutHome)
+                    console.log('"Wendy. I am home.""')
+                  }
+                else { await page.click(clickoutSeo)
+                  console.log('"Wendy. I am SEO.""')
+                }
 
   // THEN popup checkboxes appear below
   // AND I disable popup checkboxes
 
   await page.waitForSelector(deselectComparesite)
   await page.click(deselectComparesite)
+
     console.log('√ checkboxes are deselected')
 
   // WhHEN I launch search
   await page.waitForSelector(airSubmit)
   await page.click(airSubmit)
+
     console.log('√ search is launched')
 
+/*
+ -----------------------------------
+ RESULT PAGE
+ -----------------------------------
+  ThHEN result page appears
+  AND results appear
+*/
 
-  // ThHEN result page appears
-  // AND results appear
+    const departureHeader = '#liligo_content > flight-results > div > div > div > div.results-header.results-header-finished > div.results-header-container > result-header-wrapper > div > div.results-header-cities > div:nth-child(1) > span'
+    const arrivalHeader   = '#liligo_content > flight-results > div > div > div > div.results-header.results-header-finished > div.results-header-container > result-header-wrapper > div > div.results-header-cities > div:nth-child(4) > span'
+    
+    await page.waitForSelector(departureHeader)
+
+
+    let departureHeaderContent = await page.evaluate(el => el.value, await page.$(departureHeader))
+    expect(departureHeaderContent).toBe(airFromContentArray[0])
+
+      console.log('√ ' + departureHeaderContent + 'matches ' + airFromContentArray[0])
+
+    let arrivalHeaderContent = await page.evaluate(el => el.value, await page.$(arrivalHeader))
+    expect(arrivalHeaderContent).toBe(airToContentArray[0])
+
+      console.log('√ ' + arrivalHeaderContent + 'matches ' + airToContentArray[0])
+
+
   await page.waitForSelector('.travel-details-button')
   await page.click('.travel-details-button')
   await page.click('.travel-details-button')
