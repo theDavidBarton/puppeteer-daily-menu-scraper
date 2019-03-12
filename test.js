1461a2d87acf75bc5e9f9972ccf9d8ecb7229ff3
David Barton <barton.david.sv@gmail.com>
2019-03-12 21:41:51 +0100

    organize variables

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-// testing with Puppeteer example
-const puppeteer = require('puppeteer');
-const puppeteerFirefox = require('puppeteer-firefox');
-const expect = require('expect');
+    const puppeteer          = require('puppeteer');
+    const puppeteerFirefox   = require('puppeteer-firefox');
+    const expect             = require('expect');
+
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false, slowMo: 20 })
@@ -10,64 +10,29 @@ const expect = require('expect');
 
   await page.setViewport({ width: 1024, height: 768 })
 
-    console.log('√ puppeteer has launched')
+    console.log('... puppeteer has launched')
 
 
 /*
  -----------------------------------
  SELECTORS
  -----------------------------------
- selector collection
 */
 
-      const urlHomePage         = 'http://www.liligo.fr/'
-      const airFrom             = '#air-from'
-      const airTo               = '#air-to'
-      const complocFirst        = '#liligo_cl2_item_0'
-      const complocSecond       = '#liligo_cl2_item_1'
-      const airFromDate         = '#air-out-date-value'
-      const airToDate           = '#air-ret-date-value'
-      const deselectComparesite = '.hp-searchform-comparesite-selectnone'
-      const airSubmit           = '#air-submit'
+      const urlHomepage          = 'http://www.liligo.fr/'
+      const airFromTypeLetters   = 'San f'
+      const airToTypeLetters     = 'Par'
+      const airFromContentToBe   = 'San Francisco,  CA, Etats-Unis (SFO)'
+      const airToContentToBe     = 'Paris, France (CDG)'
 
-
-  await page.goto(urlHomePage , { waitUntil: 'networkidle2', timeout: 0 }) // Firefox: remove "{ waitUntil: 'networkidle2', timeout: 0 }"
-// @ @ @ GHERKIN
-    console.log('√ GIVEN I am on the homepage of ' + urlHomePage )
-
-/*
- -----------------------------------
- LOCATIONS
- -----------------------------------
-*/
-
-  await page.waitForSelector(airFrom)
-  await page.click(airFrom)
-  await page.keyboard.type('San f')
-  await page.waitFor(1000)        // make sure dropdown opens
-  await page.waitForSelector(complocFirst)
-  await page.click(complocFirst)
-  let airFromContent = await page.evaluate(el => el.value, await page.$(airFrom))
-  expect(airFromContent).toBe('San Francisco,  CA, Etats-Unis (SFO)')
-// @ @ @ GHERKIN
-    console.log('√ WHEN I set departure to ' + airFromContent)
-
-
-  await page.keyboard.type('Par')
-  await page.waitFor(1000)
-  await page.waitForSelector(complocSecond)
-  await page.click(complocSecond)
-  let airToContent = await page.evaluate(el => el.value, await page.$(airTo))
-  expect(airToContent).toBe('Paris, France (CDG)')
-// @ @ @ GHERKIN
-    console.log('√ AND I set destination to ' + airToContent)
-
-
-/*
- -----------------------------------
- DATE PICKER
- -----------------------------------
-*/
+      const airFrom              = '#air-from'
+      const airTo                = '#air-to'
+      const complocFirst         = '#liligo_cl2_item_0'
+      const complocSecond        = '#liligo_cl2_item_1'
+      const airFromDate          = '#air-out-date-value'
+      const airToDate            = '#air-ret-date-value'
+      const deselectComparesite  = '.hp-searchform-comparesite-selectnone'
+      const airSubmit            = '#air-submit'
 
       const nowDate              = '.now'
       const actualDate           = '.actual'
@@ -76,6 +41,50 @@ const expect = require('expect');
       const datePickerArrowLeft  = '.dpPrev'
       const datePickerArrowRight = '.dpNext'
       const randomFutureDate     = 'tr:nth-child(3) > td:nth-child(4)'
+
+      const clickoutHome         = '.hp-searcharea' // the searchform's background on homepage
+      const clickoutSeo          = '.sc-searchform'  // the searchform's background on SEO (sc)
+
+      const resultDetailsButton  = '.travel-details-button'
+      const cta                  = '.cta'
+
+
+  await page.goto(urlHomepage , { waitUntil: 'networkidle2', timeout: 0 }) // Firefox: remove "{ waitUntil: 'networkidle2', timeout: 0 }"
+
+/*
+ -----------------------------------
+ LOCATIONS
+ -----------------------------------
+*/
+
+  await page.waitForSelector(airFrom)
+// @ @ @ GHERKIN
+    console.log('√ GIVEN I am on the homepage of ' + urlHomepage )
+
+  await page.click(airFrom)
+  await page.keyboard.type(airFromTypeLetters)
+  await page.waitFor(1000)        // make sure dropdown opens
+  await page.waitForSelector(complocFirst)
+  await page.click(complocFirst)
+  let airFromContent = await page.evaluate(el => el.value, await page.$(airFrom))
+  expect(airFromContent).toBe(airFromContentToBe)
+// @ @ @ GHERKIN
+    console.log('√ WHEN I set departure to ' + airFromContent)
+
+  await page.keyboard.type(airToTypeLetters)
+  await page.waitFor(1000)
+  await page.waitForSelector(complocSecond)
+  await page.click(complocSecond)
+  let airToContent = await page.evaluate(el => el.value, await page.$(airTo))
+  expect(airToContent).toBe(airToContentToBe)
+// @ @ @ GHERKIN
+    console.log('√ AND I set destination to ' + airToContent)
+
+/*
+ -----------------------------------
+ DATE PICKER
+ -----------------------------------
+*/
 
   await page.waitForSelector(airFromDate)
   let airFromDateContent = await page.evaluate(el => el.innerText, await page.$(airFromDate))
@@ -92,7 +101,7 @@ const expect = require('expect');
         airFromDateContent = await page.evaluate(el => el.innerText, await page.$(airFromDate)) // format: 18 Avr. 2019 (jeudi)
         airToDateContent = await page.evaluate(el => el.innerText, await page.$(airToDate))     // format: 25 Avr. 2019 (jeudi)
 // @ @ @ GHERKIN
-          console.log('√ WHEN I select departure date: ' + airFromDateContent)
+          console.log('√ AND I select departure date: ' + airFromDateContent)
           console.log('√ THEN selected return date is: ' + airToDateContent)
 
             // validates date selection
@@ -103,9 +112,9 @@ const expect = require('expect');
 // @ @ @ GHERKIN
             console.log('----> departure day selected: ' + airFromDateSelected)
 
-              expect(airFromDateContent).toMatch(/19/)
+              expect(airFromDateContent).toMatch(/2019/)
 // @ @ @ GHERKIN
-              console.log('√ airFrom contains "19"')
+              console.log('√ airFrom contains "2019"')
 
             await page.click(airToDate)
             let airToDateSelectedMonth = await page.evaluate(el => el.innerText, await page.$(datePickerMonth1))
@@ -114,29 +123,20 @@ const expect = require('expect');
 // @ @ @ GHERKIN
             console.log('----> return day selected: ' + airToDateSelected)
 
-              expect(airToDateContent).toMatch(/19/)
+              expect(airToDateContent).toMatch(/2019/)
 // @ @ @ GHERKIN
-              console.log('√ airTo contains "19"')
+              console.log('√ airTo contains "2019"')
 
 // prepare arrays from route location elements for result page validation
+// expected format: [ 'San Francisco', ' CA', 'Etats-Unis (SFO)' ]
 let airFromContentArray = airFromContent.split(', ')
 let airToContentArray = airToContent.split(', ')
-
-/*
-expected output format:
-[ 'San Francisco', ' CA', 'Etats-Unis (SFO)' ]
-[ 'Paris', 'France (CDG)' ]
-*/
 
 /*
  -----------------------------------
  CLICKOUT (promise.race)
  -----------------------------------
 */
-
-                const clickoutHome = '.hp-searcharea' // the searchform's background on homepage
-                const clickoutSeo  = '.sc-searchform'  // the searchform's background on SEO (sc)
-
 
                 await Promise.race([
                   page.waitForSelector(clickoutHome),
@@ -146,11 +146,11 @@ expected output format:
                 if (await page.$(clickoutHome) !== null) {
                     await page.click(clickoutHome)
 // @ @ @ GHERKIN
-                    console.log('AND I clickout from homepage searchform')
+                    console.log('√ AND I clickout from homepage searchform')
                   }
                 else { await page.click(clickoutSeo)
 // @ @ @ GHERKIN
-                    console.log('AND I clickout from seo searchform')
+                    console.log('√ AND I clickout from seo searchform')
                 }
 
 
@@ -175,9 +175,10 @@ expected output format:
  -----------------------------------
 */
 
-await page.waitFor(8000)
+  await page.waitForSelector(resultDetailsButton)
 // @ @ @ GHERKIN
-    console.log('√ THEN result page appears')
+    console.log('√ THEN resultpage appears')
+    console.log('√ AND results appear')
         // source: https://github.com/GoogleChrome/puppeteer/issues/2859
         const departureHeaderSelector = (await page.$$('.results-header-city'))[0]
         const arrivalHeaderSelector = (await page.$$('.results-header-city'))[1]
@@ -192,20 +193,20 @@ await page.waitFor(8000)
 // @ @ @ GHERKIN
         console.log('√ AND destination: ' + arrivalHeaderContent + ' from result header matches ' + airToContentArray[0] + ' from homepage')
 
-// @ @ @ GHERKIN
-      console.log('√ AND results appear')
-  await page.waitForSelector('.travel-details-button')
-  await page.click('.travel-details-button')
-  await page.click('.travel-details-button')
 
+// @ @ @ GHERKIN
+      console.log('√ WHEN I click on result details')
+  await page.click(resultDetailsButton)
+// @ @ @ GHERKIN
+      console.log('√ THEN result details appear')
   await navigationPromise
 
-  await page.waitForSelector('.cta')
-  await page.click('.cta')
+  await page.waitForSelector(cta)
+  await page.click(cta)
 // @ @ @ GHERKIN
       console.log('√ WHEN I click on an offer')
   await navigationPromise
 // @ @ @ GHERKIN
-      console.log('√ THEN I see a redirection to partner site')
+      console.log('√ THEN I see a redirection to partner\'s site')
   await browser.close()
 })()
