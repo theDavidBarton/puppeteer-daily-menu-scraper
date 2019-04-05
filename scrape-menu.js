56a6b0d649e92426a182d0d5dd74b6401442340d
David Barton <barton.david.sv@gmail.com>
2019-04-06 00:39:39 +0200

    xpath solution

--- scrape-menu.js
+++ scrape-menu.js
@@ -62,13 +62,15 @@ async function scrapeMenu() {
   */
 
   // @ SUPPÉ selectors
-  const dailySuppeSelector = ''
 
   let suppeName = 'Suppé menu:'
   let suppeLength = suppeName.length
   console.log('*' + suppeName + '* \n' + '-'.repeat(suppeLength))
-  await page.goto('', { waitUntil: 'networkidle2' })
-  let dailySuppe = await page.evaluate(el => el.innerText, await page.$(dailySuppeSelector))
+  await page.goto('https://www.facebook.com/pg/bistrosuppe/posts/?ref=page_internal', { waitUntil: 'networkidle2' })
+  const dailySuppeIncludes = (await page.$x('//span[contains(text(), "Sziasztok")]'))[0]
+  console.log(dailySuppeIncludes)
+  const dailySuppeSelector = dailySuppeIncludes
+  //let dailySuppe = await page.evaluate(el => el.innerText, await page.$(dailySuppeSelector))
 
   // @ SUPPÉ print menu
   console.log('• Daily menu: ' + dailySuppe + '\n')
@@ -112,10 +114,7 @@ async function scrapeMenu() {
   // @ YAMATO Monday
   let mondayYamato
   if ((await page.$(mondayYamatoSelector)) !== null) {
-    let mondayYamatoRaw = await page.evaluate(
-      el => el.innerText,
-      await page.$(mondayYamatoSelector)
-    )
+    let mondayYamatoRaw = await page.evaluate(el => el.innerText, await page.$(mondayYamatoSelector))
     mondayYamato = mondayYamatoRaw.replace(/(\n)/gm, ', ')
   } else {
     mondayYamato = '♪"No Milk Today"♫'
@@ -124,10 +123,7 @@ async function scrapeMenu() {
   // @ YAMATO Tuesday
   let tuesdayYamato
   if ((await page.$(tuesdayYamatoSelector)) !== null) {
-    let tuesdayYamatoRaw = await page.evaluate(
-      el => el.innerText,
-      await page.$(tuesdayYamatoSelector)
-    )
+    let tuesdayYamatoRaw = await page.evaluate(el => el.innerText, await page.$(tuesdayYamatoSelector))
     tuesdayYamato = tuesdayYamatoRaw.replace(/(\n)/gm, ', ')
   } else {
     tuesdayYamato = '♪"No Milk Today"♫'
@@ -136,10 +132,7 @@ async function scrapeMenu() {
   // @ YAMATO Wednesday
   let wednesdayYamato
   if ((await page.$(wednesdayYamatoSelector)) !== null) {
-    let wednesdayYamatoRaw = await page.evaluate(
-      el => el.innerText,
-      await page.$(wednesdayYamatoSelector)
-    )
+    let wednesdayYamatoRaw = await page.evaluate(el => el.innerText, await page.$(wednesdayYamatoSelector))
     wednesdayYamato = wednesdayYamatoRaw.replace(/(\n)/gm, ', ')
   } else {
     wednesdayYamato = '♪"No Milk Today"♫'
@@ -148,10 +141,7 @@ async function scrapeMenu() {
   // @ YAMATO Thursday
   let thursdayYamato
   if ((await page.$(thursdayYamatoSelector)) !== null) {
-    let thursdayYamatoRaw = await page.evaluate(
-      el => el.innerText,
-      await page.$(thursdayYamatoSelector)
-    )
+    let thursdayYamatoRaw = await page.evaluate(el => el.innerText, await page.$(thursdayYamatoSelector))
     thursdayYamato = thursdayYamatoRaw.replace(/(\n)/gm, ', ')
   } else {
     thursdayYamato = '♪"No Milk Today"♫'
@@ -160,10 +150,7 @@ async function scrapeMenu() {
   // @ YAMATO Friday
   let fridayYamato
   if ((await page.$(fridayYamatoSelector)) !== null) {
-    let fridayYamatoRaw = await page.evaluate(
-      el => el.innerText,
-      await page.$(fridayYamatoSelector)
-    )
+    let fridayYamatoRaw = await page.evaluate(el => el.innerText, await page.$(fridayYamatoSelector))
     fridayYamato = fridayYamatoRaw.replace(/(\n)/gm, ', ')
   } else {
     fridayYamato = '♪"No Milk Today"♫'
@@ -425,12 +412,9 @@ async function scrapeMenu() {
   */
 
   // @ KORHELY selectors
-  const weeklySoupKorhelySelector =
-    '#mainDiv > div > div:nth-child(2) > section > ul > li:nth-child(1)'
-  const weeklyMainKorhelySelector =
-    '#mainDiv > div > div:nth-child(2) > section > ul > li:nth-child(2)'
-  const weeklyDessertKorhelySelector =
-    '#mainDiv > div > div:nth-child(2) > section > ul > li:nth-child(3)'
+  const weeklySoupKorhelySelector = '#mainDiv > div > div:nth-child(2) > section > ul > li:nth-child(1)'
+  const weeklyMainKorhelySelector = '#mainDiv > div > div:nth-child(2) > section > ul > li:nth-child(2)'
+  const weeklyDessertKorhelySelector = '#mainDiv > div > div:nth-child(2) > section > ul > li:nth-child(3)'
 
   let korhelyName = 'Korhely menu:'
   let korhelyLength = korhelyName.length
@@ -447,20 +431,11 @@ async function scrapeMenu() {
 
   await page.goto(linkKorhely, { waitUntil: 'networkidle2', timeout: 0 })
   // let weeklySummaryKorhely = await page.evaluate(el => el.innerText, await page.$(weeklySummaryKorhelySelector))
-  let weeklySoupKorhely = await page.evaluate(
-    el => el.innerText,
-    await page.$(weeklySoupKorhelySelector)
-  )
+  let weeklySoupKorhely = await page.evaluate(el => el.innerText, await page.$(weeklySoupKorhelySelector))
   weeklySoupKorhely = weeklySoupKorhely.replace('LEVESEK', '')
-  let weeklyMainKorhely = await page.evaluate(
-    el => el.innerText,
-    await page.$(weeklyMainKorhelySelector)
-  )
+  let weeklyMainKorhely = await page.evaluate(el => el.innerText, await page.$(weeklyMainKorhelySelector))
   weeklyMainKorhely = weeklyMainKorhely.replace('FŐÉTELEK', '')
-  let weeklyDessertKorhely = await page.evaluate(
-    el => el.innerText,
-    await page.$(weeklyDessertKorhelySelector)
-  )
+  let weeklyDessertKorhely = await page.evaluate(el => el.innerText, await page.$(weeklyDessertKorhelySelector))
   weeklyDessertKorhely = weeklyDessertKorhely.replace('DESSZERTEK', '')
 
   // @ KORHELY print menu
@@ -525,14 +500,8 @@ async function scrapeMenu() {
   let mondayKetszerecsen1
   let mondayKetszerecsen2
   if ((await page.$(mondayKetszerecsenSelector1)) !== null) {
-    mondayKetszerecsen1 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(mondayKetszerecsenSelector1)
-    )
-    mondayKetszerecsen2 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(mondayKetszerecsenSelector2)
-    )
+    mondayKetszerecsen1 = await page.evaluate(el => el.innerHTML, await page.$(mondayKetszerecsenSelector1))
+    mondayKetszerecsen2 = await page.evaluate(el => el.innerHTML, await page.$(mondayKetszerecsenSelector2))
   } else {
     mondayKetszerecsen1 = '♪"No Milk Today"♫'
     mondayKetszerecsen2 = ''
@@ -542,14 +511,8 @@ async function scrapeMenu() {
   let tuesdayKetszerecsen1
   let tuesdayKetszerecsen2
   if ((await page.$(tuesdayKetszerecsenSelector1)) !== null) {
-    tuesdayKetszerecsen1 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(tuesdayKetszerecsenSelector1)
-    )
-    tuesdayKetszerecsen2 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(tuesdayKetszerecsenSelector2)
-    )
+    tuesdayKetszerecsen1 = await page.evaluate(el => el.innerHTML, await page.$(tuesdayKetszerecsenSelector1))
+    tuesdayKetszerecsen2 = await page.evaluate(el => el.innerHTML, await page.$(tuesdayKetszerecsenSelector2))
   } else {
     tuesdayKetszerecsen1 = '♪"No Milk Today"♫'
     tuesdayKetszerecsen2 = ''
@@ -559,14 +522,8 @@ async function scrapeMenu() {
   let wednesdayKetszerecsen1
   let wednesdayKetszerecsen2
   if ((await page.$(wednesdayKetszerecsenSelector1)) !== null) {
-    wednesdayKetszerecsen1 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(wednesdayKetszerecsenSelector1)
-    )
-    wednesdayKetszerecsen2 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(wednesdayKetszerecsenSelector2)
-    )
+    wednesdayKetszerecsen1 = await page.evaluate(el => el.innerHTML, await page.$(wednesdayKetszerecsenSelector1))
+    wednesdayKetszerecsen2 = await page.evaluate(el => el.innerHTML, await page.$(wednesdayKetszerecsenSelector2))
   } else {
     wednesdayKetszerecsen1 = '♪"No Milk Today"♫'
     wednesdayKetszerecsen2 = ''
@@ -576,14 +533,8 @@ async function scrapeMenu() {
   let thursdayKetszerecsen1
   let thursdayKetszerecsen2
   if ((await page.$(thursdayKetszerecsenSelector1)) !== null) {
-    thursdayKetszerecsen1 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(thursdayKetszerecsenSelector1)
-    )
-    thursdayKetszerecsen2 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(thursdayKetszerecsenSelector2)
-    )
+    thursdayKetszerecsen1 = await page.evaluate(el => el.innerHTML, await page.$(thursdayKetszerecsenSelector1))
+    thursdayKetszerecsen2 = await page.evaluate(el => el.innerHTML, await page.$(thursdayKetszerecsenSelector2))
   } else {
     thursdayKetszerecsen1 = '♪"No Milk Today"♫'
     thursdayKetszerecsen2 = ''
@@ -593,14 +544,8 @@ async function scrapeMenu() {
   let fridayKetszerecsen1
   let fridayKetszerecsen2
   if ((await page.$(fridayKetszerecsenSelector1)) !== null) {
-    fridayKetszerecsen1 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(fridayKetszerecsenSelector1)
-    )
-    fridayKetszerecsen2 = await page.evaluate(
-      el => el.innerHTML,
-      await page.$(fridayKetszerecsenSelector2)
-    )
+    fridayKetszerecsen1 = await page.evaluate(el => el.innerHTML, await page.$(fridayKetszerecsenSelector1))
+    fridayKetszerecsen2 = await page.evaluate(el => el.innerHTML, await page.$(fridayKetszerecsenSelector2))
   } else {
     fridayKetszerecsen1 = '♪"No Milk Today"♫'
     fridayKetszerecsen2 = ''
@@ -672,21 +617,14 @@ async function scrapeMenu() {
 
   // @ FRUCCOLA selectors
   const dailyFruccolaSelector1 = '#dailymenu-holder > li.arany.today > div.soup > p.description'
-  const dailyFruccolaSelector2 =
-    '#dailymenu-holder > li.arany.today > div.main-dish > p.description'
+  const dailyFruccolaSelector2 = '#dailymenu-holder > li.arany.today > div.main-dish > p.description'
 
   let fruccolaName = 'Fruccola (Arany Janos utca) menu:'
   let fruccolaLength = fruccolaName.length
   console.log('*' + fruccolaName + '* \n' + '-'.repeat(fruccolaLength))
   await page.goto('http://fruccola.hu/hu', { waitUntil: 'networkidle2' })
-  const dailyFruccola1 = await page.evaluate(
-    el => el.innerText,
-    await page.$(dailyFruccolaSelector1)
-  )
-  const dailyFruccola2 = await page.evaluate(
-    el => el.innerText,
-    await page.$(dailyFruccolaSelector2)
-  )
+  const dailyFruccola1 = await page.evaluate(el => el.innerText, await page.$(dailyFruccolaSelector1))
+  const dailyFruccola2 = await page.evaluate(el => el.innerText, await page.$(dailyFruccolaSelector2))
 
   // @ FRUCCOLA print menu
   console.log('• Daily menu: ' + dailyFruccola1 + ', ' + dailyFruccola2 + '\n')
@@ -729,9 +667,7 @@ async function scrapeMenu() {
   })
   const dayKamra = await page.evaluate(el => el.innerText, await page.$(dayKamraSelector))
   // stores all elements with same ID, source: https://stackoverflow.com/questions/54677126/how-to-select-all-child-div-with-same-class-using-puppeteer
-  const dailyKamra = await page.$$eval(dailyKamraSelector, divs =>
-    divs.map(({ innerText }) => innerText)
-  )
+  const dailyKamra = await page.$$eval(dailyKamraSelector, divs => divs.map(({ innerText }) => innerText))
 
   // @ KAMRA print menu
   console.log('• ' + dayKamra + ' daily menu: ' + dailyKamra + '\n')
