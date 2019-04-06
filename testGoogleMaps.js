34571e4b112dfb3b23e6e9e8f474fa1099337b5b
David Barton <barton.david.sv@gmail.com>
2019-04-06 23:28:48 +0200

    Google Maps zoom wit button

--- testGoogleMaps.js
+++ testGoogleMaps.js
@@ -3,8 +3,13 @@ async function testGoogleMaps() {
   const browser = await puppeteer.launch({ headless: false, slowMo: 20 })
   const page = await browser.newPage()
 
-  await page.goto('https://maps.google.com', { waitUntil: 'networkidle0', timeout: 0 })
-
+  await page.goto('https://www.google.com/maps?hl=en', { waitUntil: 'networkidle0', timeout: 0 })
+  await page.waitForSelector('#minimap > div > div.widget-minimap > button')
+  await page.click('#minimap > div > div.widget-minimap > button')
+  for (let i = 0; i < 15; i++) {
+    await page.click('#widget-zoom-in')
+  }
+  await page.waitFor(3000)
   await page.screenshot({ path: 'tmp/maps.png' })
   await browser.close()
 }
