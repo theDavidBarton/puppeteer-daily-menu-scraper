25b92b38bbeb7f330cac08112b9a918d9afc71e6
David Barton <barton.david.sv@gmail.com>
2019-04-07 19:34:49 +0200

    refactor code

--- testGoogleMaps.js
+++ testGoogleMaps.js
@@ -1,9 +1,8 @@
-// BE CAREFUL! THERE'S A CROCODILE IN THE BASEMENT.
+// Be careful! There is a crocodile in the basement.
 const puppeteer = require('puppeteer')
 async function testGoogleMaps() {
   const browser = await puppeteer.launch({ headless: false, slowMo: 20 })
   const page = await browser.newPage()
-  const navigationPromise = page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 })
 
   await page.goto('https://www.google.com/maps?hl=en', { waitUntil: 'networkidle0', timeout: 0 })
   await page.waitForSelector('#minimap > div > div.widget-minimap > button')
@@ -12,18 +11,17 @@ async function testGoogleMaps() {
   await page.waitForSelector('#searchboxinput')
   await page.keyboard.type('Eifell Tower Paris')
   await page.keyboard.press('Enter')
-  await page.waitFor(8000)
-  await navigationPromise
+  await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 })
+  await page.waitFor(8000) // this part requires better promise handling
   await page.waitForSelector('.widget-pane-toggle-button')
   await page.click('.widget-pane-toggle-button')
   console.log('pane appeared and closed')
-
   for (let i = 0; i < 15; i++) {
     await page.click('#widget-zoom-out')
   }
   await page.waitFor(4000)
   await page.screenshot({ path: 'tmp/maps-out.png' })
-  console.log('we are at outer space')
+  console.log('We are outer space!')
   for (let i = 0; i < 30; i++) {
     await page.click('#widget-zoom-in')
   }
@@ -33,6 +31,5 @@ async function testGoogleMaps() {
   await browser.close()
 }
 testGoogleMaps()
-/*--------------
-  🐊
-*/
+//--------------
+//  🐊
