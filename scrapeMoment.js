92205eceefce3ecea61193442e68961d76548ec6
David Barton <barton.david.sv@gmail.com>
2019-04-17 00:03:29 +0200

    moment script fixed

--- scrapeMoment.js
+++ scrapeMoment.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer')
 const moment = require('moment')
 
 async function momentJs() {
-  const browser = puppeteer.launch({ headless: false, slowMo: 20 })
-  const page = browser.newPage()
+  const browser = await puppeteer.launch({ headless: false, slowMo: 20 })
+  const page = await browser.newPage()
   await page.goto('https://google.com', { waitUntil: 'networkidle0', timeout: 0 })
 
   browser.close()
