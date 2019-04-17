1fbd0b6efceea0cc5978e9df31e8ca5dcf208d92
David Barton <barton.david.sv@gmail.com>
2019-04-17 22:26:47 +0200

    finalize moment loop, fix dateformat code

--- scrapeMoment.js
+++ scrapeMoment.js
@@ -5,21 +5,22 @@ const moment = require('moment')
 async function momentJs() {
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
-  await page.goto('https://google.com', { waitUntil: 'networkidle0', timeout: 0 })
   moment.locale('hu')
-  let testDate = moment('2019. ÁPRILIS 17. 16:26', 'YYYY MMM DD').format('LLLL')
-  console.log(testDate)
+  let testStr = 'Kedd 2019.01.08.'
+  testStr = testStr.replace(/(\.)$/g, '').replace(/(\.)/g, '-')
+  let testDate = moment(testStr, 'YYYY-MM-DD').format('LLL')
+  console.log(testStr + ' --> ' + testDate)
   console.log(moment().format('LL'))
 
   await page.goto('http://chagallcafe.hu/?page_id=396', { waitUntil: 'networkidle0', timeout: 0 })
   const numberOfDaysOpened = (await page.$$('.menu-list__title')).length
   console.log('Days opened this week: ' + numberOfDaysOpened + '\n')
-
   for (let i = 0; i < numberOfDaysOpened; i++) {
     const daySelector = (await page.$$('.menu-list__title'))[i]
-    const dayTextContent = await page.evaluate(el => el.textContent, daySelector)
+    let dayTextContent = await page.evaluate(el => el.textContent, daySelector)
+    dayTextContent = dayTextContent.replace(/(\.)$/g, '').replace(/(\.)/g, '-')
     console.log('#' + (i + 1) + ' ' + dayTextContent)
-    const dayTextDate = moment(dayTextContent).add(7, 'days').format('LLLL')
+    const dayTextDate = moment(dayTextContent, 'YYYY-MM-DD').add(0, 'days').format('LL')
     console.log(dayTextDate + '\n')
   }
 
