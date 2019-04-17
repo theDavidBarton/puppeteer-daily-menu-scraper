fcf56f552be0f04b9556bf3ab723739b4ca57058
David Barton <barton.david.sv@gmail.com>
2019-04-17 17:35:02 +0200

    add working example for momentJs

--- scrapeMoment.js
+++ scrapeMoment.js
@@ -2,10 +2,14 @@ const puppeteer = require('puppeteer')
 const moment = require('moment')
 
 async function momentJs() {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 20 })
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   await page.goto('https://google.com', { waitUntil: 'networkidle0', timeout: 0 })
-
+  moment.locale('hu')
+  let testDate = moment('2019. ÁPRILIS 17. 16:26', 'YYYY MMM DD').format('LLLL')
+  moment.locale('en')
+  console.log(testDate)
+  console.log(moment().format('LL'))
   browser.close()
 }
 momentJs()
