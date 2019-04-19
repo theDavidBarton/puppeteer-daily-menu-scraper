12fb3d22bbc71219837d8de85fa393eef33a471a
David Barton <barton.david.sv@gmail.com>
2019-04-19 16:39:41 +0200

    refactor and compete scraper code

--- scrapeIMDbStoryGen.js
+++ scrapeIMDbStoryGen.js
@@ -3,21 +3,25 @@ const puppeteer = require('puppeteer')
 async function scrapeIMDbStoryGen() {
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
-
+  await page.setExtraHTTPHeaders({ 'Accept-Language': 'en-US' })
   await page.goto('https://imdb.com', { waitUntil: 'domcontentloaded', timeout: 0 })
   let urlExistsOnIMDb = await page.$('a')
   let urlCountOnIMDb = (await page.$$('a')).length
+  let urls = []
   if (urlExistsOnIMDb !== null) {
     for (let i = 0; i < urlCountOnIMDb; i++) {
       let urlOnIMDbSelector = (await page.$$('a'))[i]
       let urlOnIMDb = await page.evaluate(el => el.href, urlOnIMDbSelector)
+      let titleOnIMDb = await page.evaluate(el => el.innerText, urlOnIMDbSelector)
       urlOnIMDb = urlOnIMDb.replace(/(\?.*)/g, '').replace(/(\/$)/g, '')
-      // expected IMDb url patterns:
+      // expected IMDb url pattern:
       if (urlOnIMDb.includes('/title/')) {
-        console.log('• IMDb url: ' + urlOnIMDb)
+        urls.push(urlOnIMDb)
+        console.log('• ' + titleOnIMDb + '\nIMDb url: ' + urlOnIMDb)
       }
     }
   }
+  console.log(urls)
   browser.close()
 }
 scrapeIMDbStoryGen()
