108144bbdcf4b9880b93831c37b58a9edfb98680
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2019-04-20 23:07:21 +0200

    experiment with replace variables (#83)
    * experiment with replace variables
    
    * optimise code and finish name shuffle

--- scrapeIMDbStoryGen.js
+++ scrapeIMDbStoryGen.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 async function scrapeIMDbStoryGen() {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   await page.setExtraHTTPHeaders({ 'Accept-Language': 'en-US' })
   await page.goto('https://imdb.com', { waitUntil: 'domcontentloaded', timeout: 0 })
@@ -9,6 +9,9 @@ async function scrapeIMDbStoryGen() {
   let urlCountOnIMDb = (await page.$$('a')).length
   let urls = []
   let titles = []
+  let n
+  let plot
+  let actorNames
   if (urlExistsOnIMDb !== null) {
     for (let i = 0; i < urlCountOnIMDb; i++) {
       let urlOnIMDbSelector = (await page.$$('a'))[i]
@@ -24,16 +27,17 @@ async function scrapeIMDbStoryGen() {
       }
     }
   }
-  let n = 0
-  let plot
-  let actorNameRegex
   do {
     do {
+      n = 0
       n++
       console.log('Try #' + n)
       let random = Math.floor(Math.random() * urls.length)
       console.log('Chosen movie: ' + titles[random])
-      await page.goto(urls[random] + '/plotsummary#synopsis', { waitUntil: 'domcontentloaded', timeout: 0 })
+      await page.goto(urls[random] + '/plotsummary#synopsis', {
+        waitUntil: 'domcontentloaded',
+        timeout: 0
+      })
       plot = await page.evaluate(el => el.textContent, (await page.$$('#plot-synopsis-content'))[0])
       plot = plot.trim()
       if (!plot.includes("It looks like we don't have a Synopsis for this title yet. ")) {
@@ -42,19 +46,45 @@ async function scrapeIMDbStoryGen() {
         console.log('THERE IS NO PLOT AVAILABLE')
       }
     } while (plot.includes("It looks like we don't have a Synopsis for this title yet. "))
-    // source for matching as much as possible actor names: https://stackoverflow.com/questions/7653942/find-names-with-regular-expression
-    actorNameRegex = plot.match(
+    // matching as much as possible actor names based on IMDb patterns, regex source: https://stackoverflow.com/questions/7653942/find-names-with-regular-expression
+    actorNames = plot.match(
       /\(([A-Z]([a-z]+|\.)(?:\s+[A-Z]([a-z]+|\.))*(?:\s+[a-z][a-z\-]+){0,2}\s+[A-Z]([a-z]+|\.)+)\)/gm
     )
-    let actorNames = []
-    if (actorNameRegex !== null) {
-      actorNameRegex = actorNameRegex.toString().replace(/\(|\)/g, '')
-      actorNames.push(actorNameRegex)
+    if (actorNames !== null) {
+      actorNames = actorNames
+        .toString()
+        .replace(/\(|\)/g, '')
+        .split(',')
       console.log('\nStarring: ' + actorNames)
     } else {
       console.log('\nNO ACTORS WERE LISTED')
     }
-  } while (actorNameRegex === null)
+  } while (actorNames === null)
+  let actorNamesTwisted = [
+    'Flamand a Plummer',
+    'Tim Rothen Tomatoes',
+    'Samuel L. Shaftson',
+    'John Trabanta',
+    'Frank Whale',
+    'X-Ving Rhames',
+    'Bruce Springsteen',
+    'Uma S. Thurman',
+    'Eric Stoltzek',
+    'Christopher Walkenman',
+    'Angela Joneseyyy',
+    'Maria de Medeiros de Wolkswagen',
+    'Duane Whitewalker',
+    'Peter Greffin',
+    'Stephen Hibbert and Rupert',
+    'Quentin Tarantella',
+    'Harvey Keitel S. K.'
+  ]
+  let plotTwist = plot
+  for (let i = 0; i < actorNames.length; i++) {
+    plotTwistMatch = plotTwist.match(actorNames[i])
+    plotTwist = plotTwist.replace(plotTwistMatch, actorNamesTwisted[i])
+  }
+  console.log('\nTWISTED STORY: ' + plotTwist)
   browser.close()
 }
 scrapeIMDbStoryGen()
