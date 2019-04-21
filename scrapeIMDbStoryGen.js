c628adbc278e2b943508123200912ef726d1147e
David Barton <barton.david.sv@gmail.com>
2019-04-21 12:18:38 +0200

    introduce replacement cast scraper

--- scrapeIMDbStoryGen.js
+++ scrapeIMDbStoryGen.js
@@ -4,7 +4,7 @@ async function scrapeIMDbStoryGen() {
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   await page.setExtraHTTPHeaders({ 'Accept-Language': 'en-US' })
-  await page.goto('https://imdb.com', { waitUntil: 'domcontentloaded', timeout: 0 })
+  await page.goto('https://www.imdb.com/chart/top', { waitUntil: 'domcontentloaded', timeout: 0 })
   let urlExistsOnIMDb = await page.$('a')
   let urlCountOnIMDb = (await page.$$('a')).length
   let urls = []
@@ -12,6 +12,7 @@ async function scrapeIMDbStoryGen() {
   let n
   let plot
   let actorNames
+  let actorNamesTwisted = []
   if (urlExistsOnIMDb !== null) {
     for (let i = 0; i < urlCountOnIMDb; i++) {
       let urlOnIMDbSelector = (await page.$$('a'))[i]
@@ -32,7 +33,7 @@ async function scrapeIMDbStoryGen() {
       n = 0
       n++
       console.log('Try #' + n)
-      let random = Math.floor(Math.random() * urls.length)
+      var random = Math.floor(Math.random() * urls.length)
       console.log('Chosen movie: ' + titles[random])
       await page.goto(urls[random] + '/plotsummary#synopsis', {
         waitUntil: 'domcontentloaded',
@@ -60,25 +61,20 @@ async function scrapeIMDbStoryGen() {
       console.log('\nNO ACTORS WERE LISTED')
     }
   } while (actorNames === null)
-  let actorNamesTwisted = [
-    'Flamand a Plummer',
-    'Tim Rothen Tomatoes',
-    'Samuel L. Shaftson',
-    'John Trabanta',
-    'Frank Whale',
-    'X-Ving Rhames',
-    'Bruce Springsteen',
-    'Uma S. Thurman',
-    'Eric Stoltzek',
-    'Christopher Walkenman',
-    'Angela Joneseyyy',
-    'Maria de Medeiros de Wolkswagen',
-    'Duane Whitewalker',
-    'Peter Greffin',
-    'Stephen Hibbert and Rupert',
-    'Quentin Tarantella',
-    'Harvey Keitel S. K.'
-  ]
+  await page.goto(urls[random + 1], { waitUntil: 'domcontentloaded', timeout: 0 })
+  console.log('Chosen cast replacement: ' + titles[random + 1])
+  let castListExists = (await page.$$('tr > td'))[2]
+  // ⚠️ class = '.character'
+  if (castListExists !== null) {
+    for (let i = 2; i < 58 + 1; i += 4) {
+      let castMemberSelector = (await page.$$('tr > td'))[i]
+      let castMemberName = await page.evaluate(el => el.textContent, castMemberSelector)
+      castMemberName = castMemberName.trim().replace('\n', '')
+      actorNamesTwisted.push(castMemberName)
+    }
+  }
+  console.log(actorNamesTwisted)
+
   let plotTwist = plot
   for (let i = 0; i < actorNames.length; i++) {
     plotTwistMatch = plotTwist.match(actorNames[i])
