2edaedb9efe14f3201570b53965c8de35e9d822f
David Barton <barton.david.sv@gmail.com>
2019-04-24 21:39:19 +0200

    rewrite ocr example to async await

--- ocrExample.js
+++ ocrExample.js
@@ -1,15 +1,20 @@
 const ocrSpaceApi = require('ocr-space-api')
 
-const imageFilePath = 'https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/57606561_339523573433574_6878472617980854272_n.jpg?_nc_cat=107&_nc_ht=scontent-vie1-1.xx&oh=6ebfbc5ad394316fdaed3c349c8d9d19&oe=5D2BD627'
-
-ocrSpaceApi.parseImageFromUrl(imageFilePath, {
-  apikey: '<your_api_key_here>', // to be added via env variable later
-  language: 'hun',
-  imageFormat: 'image/png',
-  scale: true,
-  isOverlayRequired: true
-}).then(function(parsedResult) {
+const imageFilePath =
+  'https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/57606561_339523573433574_6878472617980854272_n.jpg?_nc_cat=107&_nc_ht=scontent-vie1-1.xx&oh=6ebfbc5ad394316fdaed3c349c8d9d19&oe=5D2BD627'
+// https://ocr.space/ocrapi#PostParameters
+async function ocrSpace() {
+  try {
+    let parsedResult = await ocrSpaceApi.parseImageFromUrl(imageFilePath, {
+      apikey: '<your_api_key_here>', // <your_api_key_here>
+      language: 'hun',
+      imageFormat: 'image/png',
+      scale: true,
+      isOverlayRequired: true
+    })
     console.log('parsedText: ', parsedResult.parsedText)
-  }).catch(function(err) {
-    console.log('ERROR:', err)
-  })
+  } catch (e) {
+    console.error(e)
+  }
+}
+ocrSpace()
