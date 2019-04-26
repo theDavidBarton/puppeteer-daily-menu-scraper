43f15cf1c56069c6081620ca6704bdb252f99ae4
David Barton <barton.david.sv@gmail.com>
2019-04-27 01:32:16 +0200

    OCR tables properly

--- ocrExample.js
+++ ocrExample.js
@@ -1,8 +1,7 @@
 const ocrSpaceApi = require('ocr-space-api')
 
-const imageUrl =
-  'https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/57606561_339523573433574_6878472617980854272_n.jpg?_nc_cat=107&_nc_ht=scontent-vie1-1.xx&oh=6ebfbc5ad394316fdaed3c349c8d9d19&oe=5D2BD627'
-// https://ocr.space/ocrapi#PostParameters
+const imageUrl = 'http://nokedlikifozde.hu/wp-content/uploads/172.jpg'
+  // https://ocr.space/ocrapi#PostParameters
 async function ocrSpace() {
   try {
     let parsedResult = await ocrSpaceApi.parseImageFromUrl(imageUrl, {
@@ -12,7 +11,9 @@ async function ocrSpace() {
       scale: true,
       isOverlayRequired: true
     })
-    console.log('parsedText: ', parsedResult.parsedText)
+    console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines) // full list
+    console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[11].Words) // gyümölcsleves
+
   } catch (e) {
     console.error(e)
   }
