5ad40aa6b3b3599df68051fb424836b46d6d48dc
David Barton <barton.david.sv@gmail.com>
2019-04-27 16:13:09 +0200

    Its alive Its alive

--- ocrExample.js
+++ ocrExample.js
@@ -1,7 +1,8 @@
 const ocrSpaceApi = require('ocr-space-api')
 
 const imageUrl = 'http://nokedlikifozde.hu/wp-content/uploads/172.jpg'
-  // https://ocr.space/ocrapi#PostParameters
+console.log(process.env.OCR_API_KEY + '\n')
+// https://ocr.space/ocrapi#PostParameters
 async function ocrSpace() {
   try {
     let parsedResult = await ocrSpaceApi.parseImageFromUrl(imageUrl, {
@@ -11,9 +12,31 @@ async function ocrSpace() {
       scale: true,
       isOverlayRequired: true
     })
-    console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines) // full list
-    console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[11].Words) // gyümölcsleves
-
+    // console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines) // full list
+    console.log('\n ----------------')
+    console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[72].Words) // specific word (or sentence) of table with data
+    console.log('\n ----------------')
+    console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[72].Words[0].WordText) // specific word of table
+    console.log('\n ----------------')
+    // console.log(parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines.length) // table's WordCount
+    let textOverlayLinesCount = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines.length
+    let nokedliMonday = []
+    let nokedliTuesday = []
+    let nokedliWednesday = []
+    let nokedliThursday = []
+    let nokedliFriday = []
+    for (let i = 0; i < textOverlayLinesCount; i++) {
+      let wordText = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[i].Words[0].WordText
+      let wordLeft = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[i].Words[0].Left
+      monday: if (wordLeft > 780 && wordLeft < 980) {
+        nokedliMonday.push(wordText)
+      }
+      tuesday: if (wordLeft > 1310 && wordLeft < 1520) {
+        nokedliTuesday.push(wordText)
+      }
+    }
+    console.log('- Monday: ' + nokedliMonday)
+    console.log('- Tuesday: ' + nokedliTuesday)
   } catch (e) {
     console.error(e)
   }
