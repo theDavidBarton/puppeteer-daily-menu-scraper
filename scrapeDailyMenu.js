b197a77029ad1f2e3e4c9fc0edcafacc9613e708
David Barton <barton.david.sv@gmail.com>
2019-04-28 00:19:05 +0200

    refactor daily menu scrapers

--- scrapeDailyMenu.js
+++ scrapeDailyMenu.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer')
 const moment = require('moment')
 const ocrSpaceApi = require('ocr-space-api')
+const fs = require('fs')
+const compressImages = require('compress-images')
 
 // get Day of Week
 const today = Number(moment().format('d'))
@@ -28,6 +30,136 @@ async function scrapeMenu() {
       request.continue()
     }
   })
+
+  /*
+  @ NOKEDLI
+  ------------------------------------------
+  contact info:
+  * Address: Budapest, Weiner Leó u. 17, 1065
+  * Phone: (20) 499 5832
+  -----------------------------------------
+
+  imageSelector --> imageNokedliSelector
+  * store src
+  * trim thumbnail sub for normal sized image
+  */
+
+  // @ NOKEDLI selector
+  const imageNokedliSelector = '.aligncenter'
+
+  let nokedliName = 'Nokedli menu:'
+  let weeklyNokedli
+  await page.goto('http://nokedlikifozde.hu/', { waitUntil: 'networkidle2' })
+  // @ NOKEDLI weekly
+  try {
+    let imageSelector = imageNokedliSelector
+    weeklyNokedli = await page.evaluate(el => el.src, await page.$(imageSelector))
+    weeklyNokedli = weeklyNokedli.replace('-300x212', '')
+  } catch (e) {
+    console.error(e)
+  }
+  // @ NOKEDLI download weekly menu image
+  let viewSource = await page.goto(weeklyNokedli)
+  fs.writeFile('tmp/input/weeklyNokedli.jpg', await viewSource.buffer(), function(e) {
+    if (e) {
+      return console.log(e)
+    }
+    console.log('The file was saved!')
+  })
+  await page.waitFor(5000) // make sure download ends
+
+  // @ NOKEDLI reduce image size
+  let input = 'tmp/input/weeklyNokedli.jpg'
+  let output = 'tmp/output/'
+
+  compressImages(
+    input,
+    output,
+    { compress_force: false, statistic: false, autoupdate: true },
+    false,
+    { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+    { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
+    { svg: { engine: 'svgo', command: '--multipass' } },
+    { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } }
+  )
+
+  // @ NOKEDLI OCR
+  const imagePath = 'tmp/output/weeklyNokedli.jpg'
+  try {
+    let parsedResult = await ocrSpaceApi.parseImageFromLocalFile(imagePath, {
+      apikey: process.env.OCR_API_KEY, // add app.env to your environment variables, source: https://hackernoon.com/how-to-use-environment-variables-keep-your-secret-keys-safe-secure-8b1a7877d69c
+      language: 'hun',
+      imageFormat: 'image/png',
+      scale: true,
+      isOverlayRequired: true
+    })
+
+    let textOverlayLinesCount = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines.length
+    let nokedliMonday = []
+    let nokedliMondayStr = []
+    let nokedliTuesday = []
+    let nokedliTuesdayStr = []
+    let nokedliWednesday = []
+    let nokedliWednesdayStr = []
+    let nokedliThursday = []
+    let nokedliThursdayStr = []
+    let nokedliFriday = []
+    let nokedliFridayStr = []
+
+    for (let i = 0; i < textOverlayLinesCount; i++) {
+      let textOverlayWordsCount = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[i].Words.length
+      for (let j = 0; j < textOverlayWordsCount; j++) {
+        let wordLeft = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[i].Words[0].Left
+        let wordTop = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[i].Words[0].Top
+        let wordText = parsedResult.ocrParsedResult.ParsedResults[0].TextOverlay.Lines[i].Words[j].WordText
+        if (wordTop > 520 && wordTop < 1930) {
+          monday: if (wordLeft > 780 && wordLeft < 980) {
+            nokedliMonday.push(wordText)
+            nokedliMondayStr = nokedliMonday.join(' ').split(/(?= [A-ZÁÍŰŐÜÖÚÓÉ])/g)
+            for (let k = 0; k < nokedliMondayStr.length; k++) {
+              nokedliMondayStr[k] = nokedliMondayStr[k].trim()
+            }
+          }
+          tuesday: if (wordLeft > 1310 && wordLeft < 1520) {
+            nokedliTuesday.push(wordText)
+            nokedliTuesdayStr = nokedliTuesday.join(' ').split(/(?= [A-ZÁÍŰŐÜÖÚÓÉ])/g)
+            for (let k = 0; k < nokedliTuesdayStr.length; k++) {
+              nokedliTuesdayStr[k] = nokedliTuesdayStr[k].trim()
+            }
+          }
+          wednesday: if (wordLeft > 1815 && wordLeft < 2060) {
+            nokedliWednesday.push(wordText)
+            nokedliWednesdayStr = nokedliWednesday.join(' ').split(/(?= [A-ZÁÍŰŐÜÖÚÓÉ])/g)
+            for (let k = 0; k < nokedliWednesdayStr.length; k++) {
+              nokedliWednesdayStr[k] = nokedliWednesdayStr[k].trim()
+            }
+          }
+          thursday: if (wordLeft > 2345 && wordLeft < 2620) {
+            nokedliThursday.push(wordText)
+            nokedliThursdayStr = nokedliThursday.join(' ').split(/(?= [A-ZÁÍŰŐÜÖÚÓÉ])/g)
+            for (let k = 0; k < nokedliThursdayStr.length; k++) {
+              nokedliThursdayStr[k] = nokedliThursdayStr[k].trim()
+            }
+          }
+          friday: if (wordLeft > 2880 && wordLeft < 3110) {
+            nokedliFriday.push(wordText)
+            nokedliFridayStr = nokedliFriday.join(' ').split(/(?= [A-ZÁÍŰŐÜÖÚÓÉ])/g)
+            for (let k = 0; k < nokedliFridayStr.length; k++) {
+              nokedliFridayStr[k] = nokedliFridayStr[k].trim()
+            }
+          }
+        }
+      }
+    }
+    console.log('*' + nokedliName + '* \n' + '-'.repeat(nokedliName.length))
+    console.log('• Monday: ' + nokedliMondayStr.join(', ') + '\n')
+    console.log('• Tuesday: ' + nokedliTuesdayStr.join(', ') + '\n')
+    console.log('• Wednesday: ' + nokedliWednesdayStr.join(', ') + '\n')
+    console.log('• Thursday: ' + nokedliThursdayStr.join(', ') + '\n')
+    console.log('• Friday: ' + nokedliFridayStr.join(', ') + '\n')
+  } catch (e) {
+    console.error(e)
+  }
 
   /*
   @ KATA
@@ -488,35 +620,6 @@ async function scrapeMenu() {
   console.log('*' + karcsiName + '* \n' + '-'.repeat(karcsiName.length))
   console.log('• Weekly menu: ' + weeklyKarcsi + '\n')
 
-  /*
-  @ NOKEDLI
-  ------------------------------------------
-  contact info:
-  * Address: Budapest, Weiner Leó u. 17, 1065
-  * Phone: (20) 499 5832
-  -----------------------------------------
-
-  imageSelector --> imageNokedliSelector
-  * store src
-  * trim thumbnail sub for normal sized image
-  */
-
-  // @ NOKEDLI selector
-  const imageNokedliSelector = '.aligncenter'
-
-  let nokedliName = 'Nokedli menu:'
-  await page.goto('http://nokedlikifozde.hu/', { waitUntil: 'networkidle2' })
-  // @ NOKEDLI weekly
-  try {
-    let imageSelector = imageNokedliSelector
-    let weeklyNokedli = await page.evaluate(el => el.src, await page.$(imageSelector))
-    weeklyNokedli = weeklyNokedli.replace('-300x212', '')
-
-    console.log('*' + nokedliName + '* \n' + '-'.repeat(nokedliName.length))
-    console.log('• Weekly menu: ' + weeklyNokedli + '\n')
-  } catch (e) {
-    console.error(e)
-  }
   await browser.close()
 }
 scrapeMenu()
