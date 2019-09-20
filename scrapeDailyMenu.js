005784dcde537e70fbf25c02b8dbe2fc178e66ec
David Barton <barton.david.sv@gmail.com>
2019-09-20 21:41:20 +0200

    introduce config for scrapers

--- scrapeDailyMenu.js
+++ scrapeDailyMenu.js
@@ -18,6 +18,8 @@ const puppeteer = require('puppeteer')
 const moment = require('moment')
 const request = require('request')
 const mongoDbInsertMany = require('./lib/mongoDbInsertMany')
+const activeRequiredScrapers = require('./conf/requiredScrapers.json').scrapers
+  .active
 
 // get Day of Week
 const now = moment()
@@ -118,39 +120,20 @@ async function scrapeMenu() {
     RestaurantMenuDb
   }
 
-  // require scrapers after module.exports object is declared
-  const i55 = require('./scrapers/i55')
-  const pestiDiszno = require('./scrapers/pestiDiszno')
-  const incognito = require('./scrapers/incognito')
-  const kata = require('./scrapers/kata')
-  const drop = require('./scrapers/drop')
-  const bodza = require('./scrapers/bodza')
-  const yamato = require('./scrapers/yamato')
-  const vian = require('./scrapers/vian')
-  const korhely = require('./scrapers/korhely')
-  const ketszerecsen = require('./scrapers/ketszerecsen')
-  const fruccola = require('./scrapers/fruccola')
-  const kamra = require('./scrapers/kamra')
-  const roza = require('./scrapers/roza')
-  const suppe = require('./scrapers/suppe')
-  const karcsi = require('./scrapers/karcsi')
+  // require scrapers after module.exports object is declared and launch the active ones
+  async function scraperExecuter() {
+    const promises = activeRequiredScrapers.map(async scraper => {
+      const actual = require(`./scrapers/${scraper}`)
+      try {
+        await actual.scraper()
+      } catch (e) {
+        console.error(e)
+      }
+    })
 
-  // launch scrapers
-  await i55.scraper()
-  await pestiDiszno.scraper()
-  await incognito.scraper()
-  await kata.scraper()
-  await drop.scraper()
-  await bodza.scraper()
-  await yamato.scraper()
-  await vian.scraper()
-  await korhely.scraper()
-  await ketszerecsen.scraper()
-  await fruccola.scraper()
-  await kamra.scraper()
-  await roza.scraper()
-  await suppe.scraper()
-  await karcsi.scraper()
+    await Promise.all(promises)
+  }
+  await scraperExecuter()
 
   // prepare output for submit by stringifying the object
   finalJSON = JSON.stringify(finalJSON)
@@ -165,7 +148,7 @@ async function scrapeMenu() {
   // _POST the final JSON to webhook
   request(
     {
-      url: process.env.WEBHOOK_URL_PROD,
+      url: process.env.WEBHOOK_URL_TEST,
       method: 'POST',
       json: false,
       body: finalJSON
