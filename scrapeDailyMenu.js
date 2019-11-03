1d12fb0fe971174f1d87fe4f6e48e21d8b81e543
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2019-11-03 22:09:38 +0100

    fix undeclared const in scraperExecuter (#258)

--- scrapeDailyMenu.js
+++ scrapeDailyMenu.js
@@ -112,7 +112,7 @@ async function scrapeMenu() {
   // require scrapers after module.exports object is declared and launch the active ones, see: ./conf/requiredScrapers.json
   async function scraperExecuter() {
     for (const scraper of activeRequiredScrapers) {
-      actual = require(`./scrapers/${scraper}`)
+      const actual = require(`./scrapers/${scraper}`)
       try {
         await actual.scraper()
       } catch (e) {
